9de0bc7e9f4778d72157945f30afd206d7cd980e
Luiz Ribeiro <luizribeiro@gmail.com>
2021-08-03 17:50:30 -0500

    [riscv] Simplified basic structure

--- riscv/top.v
+++ riscv/top.v
@@ -1,3 +1,6 @@
+`define NUM_BITS 8
+`define N 8
+
 module top (
   output wire gpio_2,
   output wire gpio_46,
@@ -14,41 +17,27 @@ module top (
 
   riscv p (
     .clk(clk),
-    .d0(gpio_2),
-    .d1(gpio_46),
-    .d2(gpio_47),
-    .d3(gpio_45),
-    .d4(gpio_28),
-    .d5(gpio_3),
-    .d6(gpio_4),
-    .d7(gpio_44)
+    .gpio({
+      gpio_2,
+      gpio_46,
+      gpio_47,
+      gpio_45,
+      gpio_28,
+      gpio_3,
+      gpio_4,
+      gpio_44
+    })
   );
 endmodule
 
 module riscv (
   input wire clk,
-  output wire d0,
-  output wire d1,
-  output wire d2,
-  output wire d3,
-  output wire d4,
-  output wire d5,
-  output wire d6,
-  output wire d7
+  output wire [`NUM_BITS-1:0] gpio
 );
-  localparam N = 11;
-
-  reg [N:0] counter = 'b0;
+  reg [`N:0] counter = 'b0;
   always @(posedge clk) begin
     counter <= counter + 1'b1;
   end
 
-  assign d0 = counter[3];
-  assign d1 = counter[4];
-  assign d2 = counter[5];
-  assign d3 = counter[6];
-  assign d4 = counter[7];
-  assign d5 = counter[8];
-  assign d6 = counter[9];
-  assign d7 = counter[10];
+  assign gpio = counter[`N:`N-`NUM_BITS];
 endmodule
