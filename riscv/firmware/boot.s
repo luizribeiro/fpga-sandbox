026aa3677ffc9285b4304e5cb20788bf1701afcd
Luiz Ribeiro <luizribeiro@gmail.com>
2021-08-23 22:59:46 -0500

    [riscv] Small tweaks to bootloader

--- riscv/firmware/boot.s
+++ riscv/firmware/boot.s
@@ -1,17 +1,29 @@
   .section .init, "ax"
   .global _start
 _start:
-  la sp, __initial_stack_pointer
+# device initialization
+  la sp, __stack_top
+  addi sp, sp, -4
 
+# copy .data section from ROM into RAM
   la a0, __ram_data_start
   la a1, __ram_data_end
   la a2, __rom_data_start
 _copy_data:
-  ble a1, a0, _copy_done
+  ble a1, a0, _call_main
   lw a3, 0(a2)
   sw a3, 0(a0)
   addi a0, a0, 4
   addi a2, a2, 4
   j _copy_data
-_copy_done:
-  jal zero, main
+
+# call into main()
+_call_main:
+  sw ra, 12(sp)
+  sw s0, 8(sp)
+  addi s0, sp, 16
+  call main
+
+# infinite loop in case main() returns
+_end:
+  j _end
