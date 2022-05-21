4eb538598be2dcd8d280bf1df01ecf1f3d95f26b
Luiz Ribeiro <luizribeiro@gmail.com>
2022-05-21 16:04:43 -0500

    Tweaks to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let riscv32 = pkgs.pkgsCross.riscv32-embedded;
-in pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
+in
+pkgs.mkShell {
+  packages = with pkgs; [
     apio
     riscv32.buildPackages.binutils
     riscv32.buildPackages.gcc
