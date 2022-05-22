79e977718d270654f5d9bc69255b705ce945a99c
Luiz Ribeiro <luizribeiro@gmail.com>
2022-05-22 17:56:38 -0500

    [riscv] Simplified riscv.h a bit

--- riscv/firmware/riscv.h
+++ riscv/firmware/riscv.h
@@ -3,16 +3,18 @@
 
 extern unsigned char __iodev_begin;
 
+#define IODEV(addr) (*(unsigned char *)&__iodev_begin + addr)
+
 // GPIO
-#define GPIO (*(unsigned char *)&__iodev_begin)
-#define GPIO_DIR (*((unsigned char *)&__iodev_begin + 0x1))
+#define GPIO IODEV(0x0)
+#define GPIO_DIR IODEV(0x1)
 #define INPUT 0
 #define OUTPUT 1
 void set_pin_direction(int pin, int direction);
 
 // UART
-#define UART_TX (*((unsigned char *)&__iodev_begin + 0x2))
-#define UART_STATUS (*((unsigned char *)&__iodev_begin + 0x3))
+#define UART_TX IODEV(0x2)
+#define UART_STATUS IODEV(0x3)
 int is_uart_busy();
 void uart_putc(char c);
 void uart_puts(char *s);
